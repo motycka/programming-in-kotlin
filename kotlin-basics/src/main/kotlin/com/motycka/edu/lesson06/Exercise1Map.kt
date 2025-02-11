4ed1483ae131c2b7fefa8aaf27965fd2b843142c
monikaprotivova <monika.protivova@cleverfarm.ag>
2025-02-11 10:06:46 +0700

    Update

--- kotlin-basics/src/main/kotlin/com/motycka/edu/lesson06/Exercise1Map.kt
+++ kotlin-basics/src/main/kotlin/com/motycka/edu/lesson06/Exercise1Map.kt
@@ -39,9 +39,9 @@ fun main() {
         fraction2 = imperialNames.toFractionCharacters(Fraction.IMPERIAL) + sithNames.toFractionCharacters(Fraction.SITH)
     )
 
-    val result = pairs.match().onEach(::println)
+    pairs.match().onEach(::println)
 
-    val flatResult = pairs.matchWithFlatResults().onEach(::println)
+    pairs.matchWithFlatResults().onEach(::println)
 }
 
 fun List<String>.toFractionCharacters(
